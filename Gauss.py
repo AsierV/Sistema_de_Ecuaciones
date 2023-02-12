b587ce61d17e7de0da4f794bf29563f7981658b3
AsierV <asiervillar2004@gmail.com>
2023-02-12 19:00:26 +0100

    Se ha agregado pivote al proyecto

--- Gauss.py
+++ Gauss.py
@@ -1,22 +1,44 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
-Created on Sun Feb 12 18:36:04 2023
+Created on Sun Feb  5 15:12:54 2023
 
 @author: asier
 """
+
 import numpy as np
 
-A=np.matrix([[2.0,3.0,4.0,5.0],[6.0,15.0,19.0,23.0],[8.0,42.0,60.0,70.0],[12.0,60.0,1.0,17.0]])
-b=np.array([5.0,30.0,98.0,144.0])
+A=np.matrix([[1.0,1.0,-1.0,0.0],[1.0,-1.0,0.0,1.0],[-1.0,0.0,1.0,1.0],[0.0,1.0,1.0,-1.0]])
+b=np.array([1.0,1.0,1.0,1.0])
 
 def Gauss(A,b):
-
+    
     n=np.linalg.matrix_rank(A)
     x=np.zeros(n)
     Ab=np.c_[A,b]
     for j in range(n-1):
         for i in range(j,n-1):
             a=Ab[i+1,j]
+            
+            #******************************
+            
+            if(Ab[j,j]==0):
+                f=0
+                for t in range(n-1):
+                    if(abs(Ab[j,t])>f):
+                        f=abs(Ab[j,t])
+                for o in range(n-1):
+                    if(abs(Ab[o,j])==f):
+                        break
+                
+                for i in range(n-1):
+                    d=Ab[j,i]
+                    c=Ab[o,i]
+                    
+                    Ab[o,i]=d
+                    Ab[j,i]=c
+            #******************************
+            
             m=a/(Ab[j,j])
             for k in range(n+1):
                 Ab[i+1,k]=Ab[i+1,k]-Ab[j,k]*m
